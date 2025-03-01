135062c90df2ea84a1170348c9a1eef5c38029fc
Hugo ANTAKI <hugo.antaki@edu.devinci.fr>
2025-03-01 22:31:32 +0100

    raffinage

--- Etape_1_projet_PSI/Program.cs
+++ Etape_1_projet_PSI/Program.cs
@@ -21,57 +21,12 @@ namespace Etape_1_projet_PSI
             Console.WriteLine("\n\t\tBFS de 0\n");
             etape_1.BFS(0);
 
-            Console.WriteLine(etape_1.Verification_Connexite());
-
-            //On remarque que il y a deux composantes connexes a ce graphes 
-
-            //la composante 20 - 32 ne peuvent pas former un cycle (graphe simple)
-            //regardons l'autre composante connexe 
+            Console.WriteLine(etape_1.Verification_Connexite()); 
 
             Console.WriteLine("\n\t\tnb_de_circuit de 0\n");
             Console.WriteLine(etape_1.DFS_detection_circuit(0));
-
-            //caractéristique : 
-            /* graphe non orientée
-             * non pondéré 
-             * non planaire (a verifier)
-             * non complet
-             * non connexe mais possède deux composante connexe
-             * simple 
-             * graph non bipartie (a verifier)
-             */
 
             etape_1.Draw_Graph();
         }
     }
 }
-
-/*
- for(int i = 0; i < 33; i++)
-                {
-                    Raylib.DrawCircle(
-                        300 + (int)Math.Cos(angle + angle_a_incrementer*i), 
-                        50 + (int)Math.Sin(angle + angle_a_incrementer*i), 
-                        20, Color.Red
-                    );
-                }
- */
-
-/*
-            Raylib.InitWindow(600, 600, "Graph");
-
-            Console.WriteLine((int)(250 * Math.Cos(angle_a_incrementer)));
-
-            while (!Raylib.WindowShouldClose())
-            {
-                Raylib.BeginDrawing();
-                Raylib.DrawCircle (
-                        (int)(300 + 250*Math.Sin(angle + angle_a_incrementer)),
-                        (int)(50 + 250*Math.Cos(angle + angle_a_incrementer)),
-                        20, Color.Red
-                    );
-                Raylib.EndDrawing();
-            }
-
-            Raylib.CloseWindow();
-            */
